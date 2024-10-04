e7be24674587e6525132e830836208c5e5c1d2f0
Ahmed Mursal <amursal908@gmail.com>
2024-10-04 03:03:04 +0100

    First Commit Friday 2024/10/04 added Dockerfile, updated gitignore, added code of conduct, initial project backlog, basic hello world java app

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,10 @@
+package com.napier.sem;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        String message = ("Hello");
+        System.out.println(message +" "+ "Boo yah!");
+    }
+}
