4c9cd8701f0fe72cdf238584094c361502df0b44
Ahmed Mursal <amursal908@gmail.com>
2024-10-10 02:07:07 +0100

    edited the docker compose file, edited the java file to match the docker compose file

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -28,7 +28,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/World?useSSL=false", "root", "BkQR7Aczt");
                 System.out.println("Successfully connected");
                 // Wait a bit
                 Thread.sleep(10000);
