d4f06cbdcc269361b96dd52444efe698a8f767f7
Ahmed Mursal <amursal908@gmail.com>
2024-10-12 01:51:02 +0100

    managed to extract some information from database, commented out areas of the code, added some extra tasks for testing purposes for this specific feature, next up would be using unit testing along-side implementing the queries we were asked for.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,17 +1,14 @@
 package com.napier.sem;
+import javax.management.Query;
 import java.sql.*;
 
 public class App
 {
-    public static void main(String[] args)
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
@@ -19,11 +16,9 @@ public class App
         // Connection to the database
         Connection con = null;
         int retries = 100;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
@@ -31,29 +26,28 @@ public class App
                 System.out.println("Successfully connected");
                 // Wait a bit
                 Thread.sleep(10000);
+                Queries.Statements();//calling another class before the break statement.
+                //class is named Queries and its main method is named statements
+                /**
+                 * I will be using this method to first wait for the connection with the database to be established then,
+                 *  I will send the connection string to another class to allow me to manipulate said database.
+                 * **/
                 // Exit for loop
                 break;
-            }
-            catch (SQLException sqle)
-            {
+
+            } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
 
-        if (con != null)
-        {
-            try
-            {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
---
+++ src/main/java/com/napier/sem/Queries.java
@@ -0,0 +1,47 @@
+package com.napier.sem;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+/**
+ * this class belongs to com.napier.sem and its functionality is to run the SQL Queries after ensuring that the connection works.
+ * I will implement this by using the try resources method, catch and a switch case to run each of the following requested queries.
+ * first up would be testing if this idea actually works by running a query that lists out the tables in the database then the columns under said names.
+ * and then adding the switch case statement each case will correspond to its own number and query,
+ * the cases will first give the user the prompt connecting to database, successfully  connected, thn tables in the (Database Name) along-side listing each table name.
+ * Notes/Helpful Resources:
+ * Udemy Course.Learn how to connect to a MySQL database with Java JDBC
+ * https://docs.oracle.com/javase/tutorial/jdbc/overview/index.html
+ *https://www.javatpoint.com/steps-to-connect-to-the-database-in-java
+  **/
+
+public class Queries {
+
+    public static void  Statements() throws SQLException {
+
+//set up the try statement with resources
+        try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/World", "root", "BkQR7Aczt")
+          ){
+// stmt is the statement object which is a short form of writing statement and its responsible for sending the queries to the database and executing them.
+            Statement stmt = con.createStatement();
+            String Query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'world'";
+            //rs shorter from of ResultSet which is an instance of stmt execute
+            ResultSet rs = stmt.executeQuery(Query);
+            //System.out.println(rs); instead of printing rs which will result in us printing the query we have made or specified above.
+            System.out.println("Tables in the World database:");
+            //while rs.next is used to iterate the next line until all lines are there
+            while (rs.next()) {
+              // gets the current name from the column
+                String tableName = rs.getString("table_name");
+              //this finally prints the table names.
+                System.out.println(tableName);
+            }}
+        //catch statement is used to specifically notify us of any SQL errors such as a missed line or miss matched sentence.
+catch(SQLException e) {
+    System.out.println("Error!! take a break!: " + e.getMessage());
+    // reminder not using a finally block since try resources is in use.
+}}
+
+
+}
