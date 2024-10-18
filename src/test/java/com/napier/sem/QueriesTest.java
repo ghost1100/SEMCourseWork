6dafd2616750421a84fc8b20300e0e6140b7039c
Ahmed Mursal <amursal908@gmail.com>
2024-10-18 14:30:46 +0100

    added the test files and will populate it soon

---
+++ src/test/java/com/napier/sem/QueriesTest.java
@@ -0,0 +1,24 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class QueriesTest {
+
+    /**
+     *
+     */
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void statements() {
+    }
+}
