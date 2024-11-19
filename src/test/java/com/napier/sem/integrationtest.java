44fc38715914424c954d7ae575df37988857e8f4
Ahmed Mursal <amursal908@gmail.com>
2024-11-19 01:30:12 +0000

    fixed the last integration test.

--- src/test/java/com/napier/sem/integrationtest.java
+++ src/test/java/com/napier/sem/integrationtest.java
@@ -2,20 +2,20 @@ package com.napier.sem;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
+import java.util.Scanner;
+
 public class integrationtest {
 
-    /** Quick note: docker Desktop app must be running in the back-ground for this to work.
+    /**
+     * Quick note: docker Desktop app must be running in the back-ground for this to work.
      * A unit test designed to test if the database connects or not.
      * it defined the connection link, username and password and
      * used driver manager to send them over to then proceed to say that the connection string isn't null,
-     and it also handles different types of errors by catching them at the bottom then informing the user.
-     if the connection fails then its indicated by the fail method along-side a message informing the user as to why it failed
-     below it will be another test case to ensure that we can extract information out of this database.
+     * and it also handles different types of errors by catching them at the bottom then informing the user.
+     * if the connection fails then its indicated by the fail method along-side a message informing the user as to why it failed
+     * below it will be another test case to ensure that we can extract information out of this database.
      */
 
     @Test
@@ -55,17 +55,52 @@ public class integrationtest {
                 String tableName = rs.getString("table_name");
 
                 System.out.println(tableName);
-                assertNotNull(tableName,"Table Name Should Not Be Null");
+                assertNotNull(tableName, "Table Name Should Not Be Null");
                 TableCount++;
             }
             assertTrue(TableCount > 0);
             System.out.println("Connection and Data Extraction Was A Success. :)");
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
 
-    }}
+    }
+    @Test
+    public void TestUserInputAcceptance() {//issue with this test is that it doesn't work the compiler is in view only mode so itd be best to assign a static variable with the city name and change it before running the test to see if it works
+
+        String TestCityName = "London" ;
+
+        String query = "SELECT ID, Name, CountryCode, District, Population FROM city WHERE Name LIKE ?";// the ? is used as a place-holder to mark where the scanners input will be placed
+//database information
+        String jdbcurl = "jdbc:mysql://localhost:3306/world";
+        String username = "root";
+        String password = "BkQR7Aczt";
+        int Count = 0;
+        //try the method used to link database information with the driver manager.
+        try (Connection Con = DriverManager.getConnection(jdbcurl, username, password);
+             PreparedStatement pstmt = Con.prepareStatement(query)) {
+            // this is saying the prepared statement is equal to the query plus the users input which relies on the scanner object
+            pstmt.setString(1, "%" + TestCityName + "%");// the % is used for pattern matching the pattern used here is name as long as the letters match it shouldn't be concerned if the name is in upper or lower case.
+//the parameter index is one meaning this is the one and only input we will accept from the user at the moment we can tinker about with that later to add more features when the join elements are implemented.
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " ID" + ", " + rs.getString(2) + " Name" + ", " + rs.getString(3) + " CountryCode" + ", " + rs.getString(4) + " District" + ", " + rs.getString(5) + " Population");
+                Count++;
+            }
+            assertTrue(Count > 0);
+            System.out.println("Connection and Data Extraction Was A Success. :)");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+    }
+
+}
+
+
 
 
