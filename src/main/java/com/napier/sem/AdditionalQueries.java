195700543bc9fb37af7271f447585b87dfcc8cea
Ahmed Mursal <amursal908@gmail.com>
2024-12-04 12:58:34 +0000

    Saving progress

--- src/main/java/com/napier/sem/AdditionalQueries.java
+++ src/main/java/com/napier/sem/AdditionalQueries.java
@@ -22,7 +22,8 @@ public class AdditionalQueries {
         System.out.println("Additional Queries available to user");
         System.out.println("1.(City Report) Search based on city name");
         System.out.println("2.(Country Report) Search based on country name");
-        System.out.println("3. Exit Application ");
+        System.out.println("3.(Population Report) Search population based on user input");
+        System.out.println("4. Exit Application ");
 
         Run_query();
     }
@@ -41,6 +42,11 @@ public class AdditionalQueries {
                     DisplayAdditionalQueries();
                     break;
             case 3:
+                Population_Query();
+                DisplayAdditionalQueries();
+                break;
+
+            case 4:
                 try {
                     System.out.println("Exiting Additional Queries");
                     Thread.sleep(2000);
@@ -57,6 +63,44 @@ public class AdditionalQueries {
 
 
     }
+
+
+
+    public static void Population_Query(){
+
+        System.out.println("Population Queries");
+        System.out.println("1. The name of the continent/region/country based on city name");// works well!
+        System.out.println("2. The total population of the continent/region/country.");//done works well!
+        System.out.println("3. The total population of the continent/region/country living in cities (including a %)");
+        System.out.println("4. The total population of the continent/region/country not living in cities (including a %).");
+
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        sc.nextLine();
+        switch (choice){
+            case 1:
+                City_Population_Queries();
+                DisplayAdditionalQueries();
+                break;
+                case 2:
+                    CountryQueries();
+                    DisplayAdditionalQueries();
+                    break;
+                    case 3:
+                        Population3();
+                        DisplayAdditionalQueries();
+                        break;
+                        case 4:
+                            Population4();
+                            DisplayAdditionalQueries();
+                            break;
+                    default:
+                        System.out.println("Please enter a valid choice");
+                        break;
+        }
+    }
+
+
 
     // Country query responsible for generating a report with the Country name, Continent,
     // Region, population and the Capital of the country that will be provided by the user.
@@ -112,7 +156,44 @@ public static void CountryQueries(){
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-    }}
+    }
+
+    private static void City_Population_Queries() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter City Name: ");
+        String Name = sc.nextLine();
+        String query = " SELECT city.Name AS CityName, country.Name AS Country,country.Region as Region, city.Population FROM city LEFT JOIN country ON city.CountryCode = country.Code WHERE city.Name LIKE ?;";
+        // Database connection and execution
+        try (Connection Con = DriverManager.getConnection(DatabaseConfig.jdbcurl(), DatabaseConfig.username(), DatabaseConfig.password());
+             PreparedStatement pstmt = Con.prepareStatement(query)) {
+            pstmt.setString(1, "%" + Name + "%");
+            // Execute query
+            ResultSet rs = pstmt.executeQuery();
+            // Process results
+            while (rs.next()) {
+                System.out.println("Name: " + rs.getString("CityName"));
+                System.out.println("Country: " + rs.getString("Country"));
+                System.out.println("Region: " + rs.getString("Region"));
+                System.out.println("Population: " + rs.getInt("Population"));
+                System.out.println("/ :)----------------------------------------------------:/");
+                // added a nice looking spacer.
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+// The total population of the continent/region/country living in cities (including a %)
+public static void Population3(){
+        Scanner sc = new Scanner(System.in);
+
+}
+//The total population of the continent/region/country not living in cities (including a %).
+    public static void Population4(){
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
 
 /// note to future developers the main class sends the user automatically
 /// to display queries which you must modify
